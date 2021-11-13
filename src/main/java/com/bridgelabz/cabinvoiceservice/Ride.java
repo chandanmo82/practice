f5896758cedba8d5e7997b732a5f3b561f9de71e
Your Name <you@example.com>
2021-11-13 07:51:00 +0530

    Initial commit

---
+++ src/main/java/com/bridgelabz/cabinvoiceservice/Ride.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.cabinvoiceservice;
+
+public class Ride {
+
+    public double distance;
+    public int time;
+
+    Ride(double distance, int time) {
+        this.distance = distance;
+        this.time = time;
+    }
+}
